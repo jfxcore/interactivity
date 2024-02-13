58bf4cd079226f21dd9395f716ace08772eac79b
mstr2 <43553916+mstr2@users.noreply.github.com>
2024-02-13 19:53:40 +0100

    copyright year

--- src/main/java/org/jfxcore/interaction/Trigger.java
+++ src/main/java/org/jfxcore/interaction/Trigger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, JFXcore. All rights reserved.
+ * Copyright (c) 2023, 2024, JFXcore. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
